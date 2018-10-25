279fb6561b6c4782e84ce02fc7ec477cac1102d2
Cristiana3 <44020216+Cristiana3@users.noreply.github.com>
2018-10-25 13:52:27 +0200

    Revisione db
    Aggiornamento db.

--- Youth-Server/youthClub_db.sql
+++ Youth-Server/youthClub_db.sql
@@ -31,6 +31,15 @@ CREATE TABLE `account` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `comune`
 --
 
@@ -38,14 +47,23 @@ DROP TABLE IF EXISTS `comune`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comune` (
-  `CAP` int(11) NOT NULL,
-  `Nome` varchar(30) NOT NULL,
-  `Provincia` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`CAP`),
-  KEY `Nome_Provincia` (`Provincia`),
-  CONSTRAINT `Nome_Provincia` FOREIGN KEY (`Provincia`) REFERENCES `province` (`Nome`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `ID` int(11) NOT NULL,
+  `ID_Provincia` int(11) NOT NULL,
+  `Nome` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_Provincia` (`ID_Provincia`),
+  CONSTRAINT `ID_Provincia` FOREIGN KEY (`ID_Provincia`) REFERENCES `province` (`ID_Provincia`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comune`
+--
+
+LOCK TABLES `comune` WRITE;
+/*!40000 ALTER TABLE `comune` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comune` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `locale`
@@ -56,15 +74,27 @@ DROP TABLE IF EXISTS `locale`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `locale` (
   `ID` int(11) NOT NULL,
+  `ID_Comune` int(11) DEFAULT NULL,
   `Nome` varchar(30) NOT NULL,
   `Via` varchar(45) NOT NULL,
   `Numero_telefono` varchar(45) DEFAULT NULL,
-  `CAP_Comune` int(11) DEFAULT NULL,
+  `Sito_web` varchar(45) DEFAULT NULL,
+  `Numero_votanti` int(11) DEFAULT NULL,
+  `Tot_voti` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  KEY `CAP_Comune` (`CAP_Comune`),
-  CONSTRAINT `CAP_Comune` FOREIGN KEY (`CAP_Comune`) REFERENCES `comune` (`CAP`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `ID_Comune` (`ID_Comune`),
+  CONSTRAINT `ID_Comune` FOREIGN KEY (`ID_Comune`) REFERENCES `comune` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locale`
+--
+
+LOCK TABLES `locale` WRITE;
+/*!40000 ALTER TABLE `locale` DISABLE KEYS */;
+/*!40000 ALTER TABLE `locale` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `nazioni`
@@ -80,6 +110,15 @@ CREATE TABLE `nazioni` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `nazioni`
+--
+
+LOCK TABLES `nazioni` WRITE;
+/*!40000 ALTER TABLE `nazioni` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nazioni` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `province`
 --
 
@@ -87,13 +126,23 @@ DROP TABLE IF EXISTS `province`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `province` (
-  `Nome` varchar(30) NOT NULL,
-  `Nome_Regione` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`Nome`),
+  `ID_Provincia` int(11) NOT NULL,
+  `Nome` varchar(45) NOT NULL,
+  `Nome_Regione` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID_Provincia`),
   KEY `Nome_Regione` (`Nome_Regione`),
-  CONSTRAINT `Nome_Regione` FOREIGN KEY (`Nome_Regione`) REFERENCES `regione` (`Nome_Nazione`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `Nome_Regione` FOREIGN KEY (`Nome_Regione`) REFERENCES `regione` (`Nome`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `province`
+--
+
+LOCK TABLES `province` WRITE;
+/*!40000 ALTER TABLE `province` DISABLE KEYS */;
+/*!40000 ALTER TABLE `province` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `recensioni`
@@ -103,19 +152,31 @@ DROP TABLE IF EXISTS `recensioni`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `recensioni` (
-  `ID_Locale` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
   `Account_ID` varchar(30) DEFAULT NULL,
+  `ID_Locale` int(11) NOT NULL,
   `Testo` varchar(45) DEFAULT NULL,
   `Titolo_recensione` varchar(45) DEFAULT NULL,
   `Voto` int(11) DEFAULT NULL,
   `Voto_Servizio` int(11) DEFAULT NULL,
   `Voto_qualità_prezzo` int(11) DEFAULT NULL,
   `Voto_cibo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID_Locale`),
+  PRIMARY KEY (`ID`),
   KEY `Account_ID` (`Account_ID`),
-  CONSTRAINT `Account_ID` FOREIGN KEY (`Account_ID`) REFERENCES `account` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `ID_Locale` (`ID_Locale`),
+  CONSTRAINT `Account_ID` FOREIGN KEY (`Account_ID`) REFERENCES `account` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `ID_Locale` FOREIGN KEY (`ID_Locale`) REFERENCES `locale` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recensioni`
+--
+
+LOCK TABLES `recensioni` WRITE;
+/*!40000 ALTER TABLE `recensioni` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recensioni` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `regione`
@@ -127,10 +188,20 @@ DROP TABLE IF EXISTS `regione`;
 CREATE TABLE `regione` (
   `Nome` varchar(20) NOT NULL,
   `Nome_Nazione` varchar(45) NOT NULL,
-  PRIMARY KEY (`Nome_Nazione`),
+  PRIMARY KEY (`Nome`),
+  KEY `Nome_Nazione` (`Nome_Nazione`),
   CONSTRAINT `Nome_Nazione` FOREIGN KEY (`Nome_Nazione`) REFERENCES `nazioni` (`Nome`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `regione`
+--
+
+LOCK TABLES `regione` WRITE;
+/*!40000 ALTER TABLE `regione` DISABLE KEYS */;
+/*!40000 ALTER TABLE `regione` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -141,4 +212,4 @@ CREATE TABLE `regione` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-25  1:34:02
+-- Dump completed on 2018-10-25 13:47:22
